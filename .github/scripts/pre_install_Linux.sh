d0cd7bd04184dd1b5667ab5394935133460f9ec6
Grant Gardner <grant@lastweekend.com.au>
2023-01-02 18:19:38 +1100

    more quiet

--- .github/scripts/pre_install_Linux.sh
+++ .github/scripts/pre_install_Linux.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set +x
 echo "PreInstall Linux for FUSE_PKG=${FUSE_PKG}"
-sudo apt-get update -y
-sudo apt-get install pkg-config ${FUSE_PKG} libffi-dev gcc make
+sudo apt-get update -y -q
+sudo apt-get -q -y install pkg-config ${FUSE_PKG} libffi-dev gcc make
 sudo modprobe -v fuse
 sudo chmod 666 /dev/fuse && ls -l /dev/fuse
 sudo chown root:$USER /etc/fuse.conf && ls -l /etc/fuse.conf
