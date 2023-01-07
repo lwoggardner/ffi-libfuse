fcc2dd46f47173ce2fa50149d5ba71bb0989b3d7
Grant Gardner <grant@lastweekend.com.au>
2023-01-07 17:50:29 +1100

    chore: fix release-please action

--- .github/scripts/patch_bump.sh
+++ .github/scripts/patch_bump.sh
@@ -11,12 +11,11 @@ NEW_SEMVER="${arrSEMVER[0]}.${arrSEMVER[1]}.$((arrSEMVER[2]+1))"
 echo "Updating ${VERSION_FILE} to ${NEW_SEMVER}"
 sed -i "s/VERSION = '.*'/VERSION = '${NEW_SEMVER}'/" ${VERSION_FILE}
 
+if [ -n "${GITHUB_USER_NAME}" ]; then
+  git config user.name "${GITHUB_USER_NAME}"
+  git config user.email "<>"
+fi
+
 : ${COMMIT_PREFIX:="chore: "}
 git commit -m "${COMMIT_PREFIX}patch-bump to ${NEW_SEMVER}" ${VERSION_FILE}
-
-echo "Updating git config with user.name '${GITHUB_USER_NAME}'"
-if [ -n "${GITHUB_USER_NAME}" ]; then
-  git config --global user.name "${GITHUB_USER_NAME}"
-  git config --global user.email "<>"
-fi
 git push
