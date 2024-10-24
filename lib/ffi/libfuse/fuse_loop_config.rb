3cc02fa4ca6a5811ea0ef35ed3852d1860a651f0
Grant Gardner <grant@lastweekend.com.au>
2024-10-24 18:10:25 +1100

    fix: upstream ABI changes libfuse 3.12+ #26

--- lib/ffi/libfuse/fuse_loop_config.rb
+++ lib/ffi/libfuse/fuse_loop_config.rb
@@ -15,10 +15,20 @@ module FFI
     class FuseLoopConfig < FFI::Struct
       include(FFI::Accessors)
 
-      layout(
-        clone_fd: :bool_int,
-        max_idle_threads: :int
-      )
+      STRUCT_VERSION = (FUSE_MAJOR_VERSION > 3 || (FUSE_MAJOR_VERSION == 3 && FUSE_MINOR_VERSION >= 12) ? 2 : 1)
+      if STRUCT_VERSION > 1
+        layout(
+          version_id: :int,
+          clone_fd: :bool_int,
+          max_idle_threads: :int,
+          max_threads: :uint
+        )
+      else
+        layout(
+          clone_fd: :bool_int,
+          max_idle_threads: :int
+        )
+      end
 
       # @!attribute [rw] clone_fd?
       #   whether to use separate device fds for each thread (may increase performance)
@@ -33,9 +43,38 @@ module FFI
       #   Adjusting this has performance implications; a very small number of threads in the pool will cause a lot of
       #    thread creation and deletion overhead and performance may suffer. When set to 0, a new thread will be created
       #    to service every operation.
-      #
+      #   @deprecated at Fuse 3.12. Use max_threads instead
       #   @return [Integer] the maximum number of threads to leave idle
       ffi_attr_accessor(:max_idle_threads)
+
+      # @!attribute [rw] max_threads
+      #   @return [Integer]
+      #   @since Fuse 3.12
+      ffi_attr_accessor(:max_threads) if STRUCT_VERSION > 1
+
+      class << self
+        def create(**opts)
+          cfg =
+            if STRUCT_VERSION >= 2
+              cfg = Libfuse.fuse_loop_cfg_create
+              ObjectSpace.define_finalizer(cfg, finalizer(cfg.to_ptr))
+              cfg
+            else
+              FuseLoopConfig.new
+            end
+
+          cfg.fill(**opts.select { |k, _| ffi_public_attr_writers.include?(k) })
+        end
+
+        def finalizer(ptr)
+          proc { |_| Libfuse.fuse_loop_cfg_destroy(ptr) }
+        end
+
+        if FuseLoopConfig::STRUCT_VERSION >= 2
+          Libfuse.attach_function :fuse_loop_cfg_create, [], FuseLoopConfig.by_ref
+          Libfuse.attach_function :fuse_loop_cfg_destroy, [:pointer], :void
+        end
+      end
     end
   end
 end
